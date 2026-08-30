260ae09083f4a74e4a8e6d1df7c3ec5a443a5f4e
Gomagg <emmanueloluwagbemi99@gmail.com>
2026-08-30 06:07:57 +0100

    fix: add webpack alias for @ path resolution on Render

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   experimental: { serverActions: { bodySizeLimit: "2mb" } },
   images: { remotePatterns: [] },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
 };
 module.exports = nextConfig;
